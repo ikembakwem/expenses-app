cd530aaa9875cda07a70037548b0895562afa2d0
Ikechukwu Mbakwem <ikembakwem@gmail.com>
2022-11-14 22:06:04 +0100

    Output dynamic data on component

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,13 +1,19 @@
 import './ExpenseItem.css';
 
-const ExpenseItem = () => (
-  <div className="expense-item">
-    <div>November 14th 2022</div>
-    <div className="expense-item__description">
-      <h2>Car Insurance</h2>
-      <div className="expense-item__price">$294.67</div>
+const ExpenseItem = () => {
+  const expenseDate = new Date(14, 3, 2022);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 294.67;
+
+  return (
+    <div className="expense-item">
+      <div>{expenseDate}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default ExpenseItem;
