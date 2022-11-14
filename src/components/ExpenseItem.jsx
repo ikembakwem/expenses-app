c692e354279bbb85e400889175f34c5b64a98f2a
Ikechukwu Mbakwem <ikembakwem@gmail.com>
2022-11-14 22:32:54 +0100

    Split ExpenseItem component and created ExpenseDate component

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,19 +1,15 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
-const ExpenseItem = ({ title, amount, date }) => {
-  //   const expenseDate = new Date(2022, 10, 14);
-  //   const expenseTitle = 'Car Insurance';
-  //   const expenseAmount = 294.67;
+const ExpenseItem = ({ title, amount, date }) => (
+  <div className="expense-item">
+    <ExpenseDate date={date} />
 
-  return (
-    <div className="expense-item">
-      <div>{date.toISOString()}</div>
-      <div className="expense-item__description">
-        <h2>{title}</h2>
-        <div className="expense-item__price">${amount}</div>
-      </div>
+    <div className="expense-item__description">
+      <h2>{title}</h2>
+      <div className="expense-item__price">${amount}</div>
     </div>
-  );
-};
+  </div>
+);
 
 export default ExpenseItem;
